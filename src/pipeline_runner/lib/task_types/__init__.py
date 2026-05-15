faa54c3ce846a37873a384f0a605bb29cd539816
Jason Poage <jason@jasonpoage.com>
2026-05-15 11:33:46 -0500

    modified:   src/pipeline_runner/lib/task_types/__init__.py

--- src/pipeline_runner/lib/task_types/__init__.py
+++ src/pipeline_runner/lib/task_types/__init__.py
@@ -1,4 +1,5 @@
+from .suite_sub_task import SuiteSubTask
 from .suite_task import SuiteTask
 from .task import Task
 
-__all__ = ["SuiteTask", "Task"]
+__all__ = ["SuiteTask", "Task", "SuiteSubTask"]
